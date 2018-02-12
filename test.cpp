3c6d8a695cf2fc49d91fab04b02cd8d2bf896226
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-02-12 18:24:49 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+string txt;
+int main()
+{
+  while (true) {
+    cout << "Type something. ";
+    cin >> txt;
+    cout << "You typed '" << txt << "'";
+  }
+  return 0;
+}
